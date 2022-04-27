dab31306cd8dd096ca3368ca0226f4eb178095ef
Onkar Madhekar <onkarmadhekar22@gmail.com>
2022-04-27 12:04:02 +0530

    Added Backend Code For New Cutomer Token and Attend Button

--- app/src/main/java/com/example/walk_in_sale/constants/Constant.java
+++ app/src/main/java/com/example/walk_in_sale/constants/Constant.java
@@ -1,9 +1,16 @@
 package com.example.walk_in_sale.constants;
 
 public class Constant {
+
     final String URL = "http://3.109.59.248:";
     final String PORT = "8080";
     final  String USER_LOGIN = "/user/login";
+    final  String GET_TOKEN = "/counter/getCurrentValue?sequenceName=tokenNumber";
+    final String UPDATE_TOKEN = "/token/";
+
+    public String getUPDATE_TOKEN() {
+        return UPDATE_TOKEN;
+    }
 
     public String getURL() {
         return URL;
@@ -15,5 +22,9 @@ public class Constant {
 
     public String getUSER_LOGIN() {
         return USER_LOGIN;
+    }
+
+    public String getGET_TOKEN() {
+        return GET_TOKEN;
     }
 }
--- app/src/main/java/com/example/walk_in_sale/tokenNumberCreation.java
+++ app/src/main/java/com/example/walk_in_sale/tokenNumberCreation.java
@@ -4,16 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.walk_in_sale.constants.Constant;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class tokenNumberCreation extends AppCompatActivity {
 
     TextView userName, userDescription, tokenNumber;
     SharedPreferences userSP;
     Button backBTN, attendTokenBTN, leaveTokenBTN;
+    Constant constant,constant_new;
+    RequestQueue requestQueue,requestQueueUpdate;
+    JsonObjectRequest tokenRequest,tokenUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +47,9 @@ public class tokenNumberCreation extends AppCompatActivity {
         userSP = getSharedPreferences("userMetadata", MODE_PRIVATE);
         userName.setText(userSP.getString("userName", ""));
         userDescription.setText(userSP.getString("userEmail", ""));
-        tokenNumber.setText("#"+"123");
+        getToken();
+
+
 
         //backBTN code
         backBTN.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +66,9 @@ public class tokenNumberCreation extends AppCompatActivity {
                 //Intent createTokenIntent = new Intent(getApplicationContext(), createToken.class);
                 //startActivity(createTokenIntent);
                 Toast.makeText(getApplicationContext(), "Clicked on attend", Toast.LENGTH_SHORT).show();
+                String getTokenText = tokenNumber.getText().toString();
+                String userNameText = userName.getText().toString();
+                updateToken(getTokenText,userNameText);
             }
         });
         //existinTokenBTN code
@@ -59,5 +80,65 @@ public class tokenNumberCreation extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Clicked on leave", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void getToken(){
+
+        constant = new Constant();
+        // Initializing Volley
+        requestQueue = Volley.newRequestQueue(this);
+
+        // URL
+        String url = constant.getURL() + constant.getPORT() + constant.getGET_TOKEN();
+        JSONObject object = new JSONObject();
+        // Initializing Object Request
+        tokenRequest = new JsonObjectRequest(Request.Method.GET, url,object, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                //Log.d("String Request Response",response.toString());
+                try {
+                    tokenNumber.setText(response.getString("counter"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("String Request Error",error.toString());
+                tokenNumber.setText("---");
+            }
+        });
+        requestQueue.add(tokenRequest);
+
+    }
+
+    private void updateToken(String getTokenText,String userNameText){
+        constant_new = new Constant();
+        // Initializing Request Queue
+        requestQueueUpdate = Volley.newRequestQueue(this);
+        // URL
+        String update_url = constant_new.getURL() + constant_new.getPORT() + constant_new.getUPDATE_TOKEN();
+        // JSON OBJECT FOR POST
+        JSONObject updatedData = new JSONObject();
+        try {
+            updatedData.put("username",userNameText);
+            updatedData.put("tokenNumber",getTokenText);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        tokenUpdate = new JsonObjectRequest(Request.Method.POST, update_url, updatedData, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Update Token Response",response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Update Token Response",error.toString());
+            }
+        });
+        requestQueueUpdate.add(tokenUpdate);
     }
 }
