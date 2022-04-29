f9967ae70da0f622ab84dd47fabe349d49872a2b
ShreyasKulkarni899 <sskulkarni899@gmail.com>
2022-04-30 01:20:09 +0530

    Started working on the token dashboard with the products in it- Shreyas

---
+++ app/src/main/java/com/example/walk_in_sale/tokenDashboard.java
@@ -0,0 +1,14 @@
+package com.example.walk_in_sale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tokenDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_token_dashboard);
+    }
+}
